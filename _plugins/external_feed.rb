776404b2c8460f69ae82167726c4cf816d8fb960
Cecil Williams <cecil.g.williams@gmail.com>
2017-12-06 17:29:08 -0600

    added feed for personal blog

--- _plugins/external_feed.rb
+++ _plugins/external_feed.rb
@@ -7,11 +7,10 @@ module Jekyll
     safe true
     priority :high
     def generate(site)
-      jekyll_coll = Jekyll::Collection.new(site, 'external_feed')
-      site.collections['external_feed'] = jekyll_coll
-     
-      url = "https://blogs.sourceallies.com/author/cwilliams/feed"
-
+      
+        sai_feed = Jekyll::Collection.new(site, 'sai_feed')
+        site.collections['sai_feed'] = sai_feed
+        url = "https://blogs.sourceallies.com/author/cwilliams/feed"
         open(url) do |rss|
             feed = RSS::Parser.parse(rss)
             feed.items.each do |item|
@@ -20,15 +19,38 @@ module Jekyll
                 link = item.link
                 pubdate = item.pubDate
                 newdate = Date.parse(pubdate.to_s).strftime("%B %d, %Y")
-                path = "./_external_feed/" + title + ".md"
+                path = "./_sai_feed/" + title + ".md"
                 path = site.in_source_dir(path)
-                doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
+                doc = Jekyll::Document.new(path, { :site => site, :collection => sai_feed })
                 doc.data['title'] = title
                 doc.data['blog'] = "Source Allies Blog"
                 doc.data['desc'] = desc
                 doc.data['link'] = link
                 doc.data['date'] = newdate
-                jekyll_coll.docs << doc
+                sai_feed.docs << doc
+            end
+        end
+
+        cgw_feed = Jekyll::Collection.new(site, 'cgw_feed')
+        site.collections['cgw_feed'] = cgw_feed
+        url = "https://cecilgwilliams.wordpress.com/feed"
+        open(url) do |rss|
+            feed = RSS::Parser.parse(rss)
+            feed.items.each do |item|
+                title = item.title
+                desc = item.description
+                link = item.link
+                pubdate = item.pubDate
+                newdate = Date.parse(pubdate.to_s).strftime("%B %d, %Y")
+                path = "./_cgw_feed/" + title + ".md"
+                path = site.in_source_dir(path)
+                doc = Jekyll::Document.new(path, { :site => site, :collection => cgw_feed })
+                doc.data['title'] = title
+                doc.data['blog'] = "Cecil G. Williams Blog"
+                doc.data['desc'] = desc
+                doc.data['link'] = link
+                doc.data['date'] = newdate
+                cgw_feed.docs << doc
             end
         end
 
