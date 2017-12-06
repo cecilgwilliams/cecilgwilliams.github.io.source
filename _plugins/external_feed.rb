e5d1e5bd7ee551e1006d956de299ee3033e30860
Cecil Williams <cecil.g.williams@gmail.com>
2017-12-06 17:52:22 -0600

    added featured post collection

--- _plugins/external_feed.rb
+++ _plugins/external_feed.rb
@@ -8,10 +8,18 @@ module Jekyll
     priority :high
     def generate(site)
       
+        featured_feed = Jekyll::Collection.new(site, 'featured_feed')
+        site.collections['featured_feed'] = featured_feed
+
         sai_feed = Jekyll::Collection.new(site, 'sai_feed')
         site.collections['sai_feed'] = sai_feed
-        url = "https://blogs.sourceallies.com/author/cwilliams/feed"
-        open(url) do |rss|
+        feed_url = "https://blogs.sourceallies.com/author/cwilliams/feed"
+        featured_urls = [
+        "https://blogs.sourceallies.com/2016/10/7-ways-to-go-beyond-agile/",
+        "https://blogs.sourceallies.com/2014/06/updated-tdd-mantra/",
+        "https://blogs.sourceallies.com/2013/12/a-better-analogy-for-agile-software-development/"
+        ]
+        open(feed_url) do |rss|
             feed = RSS::Parser.parse(rss)
             feed.items.each do |item|
                 title = item.title
@@ -19,22 +27,35 @@ module Jekyll
                 link = item.link
                 pubdate = item.pubDate
                 newdate = Date.parse(pubdate.to_s).strftime("%B %d, %Y")
-                path = "./_sai_feed/" + title + ".md"
-                path = site.in_source_dir(path)
-                doc = Jekyll::Document.new(path, { :site => site, :collection => sai_feed })
-                doc.data['title'] = title
-                doc.data['blog'] = "Source Allies Blog"
-                doc.data['desc'] = desc
-                doc.data['link'] = link
-                doc.data['date'] = newdate
-                sai_feed.docs << doc
+                
+                if featured_urls.include?(link)
+                    path = "./_featured_feed/" + title + ".md"
+                    path = site.in_source_dir(path)
+                    doc = Jekyll::Document.new(path, { :site => site, :collection => featured_feed })
+                    doc.data['title'] = title
+                    doc.data['blog'] = "Source Allies Blog"
+                    doc.data['desc'] = desc
+                    doc.data['link'] = link
+                    doc.data['date'] = newdate
+                    featured_feed.docs << doc
+                else
+                    path = "./_sai_feed/" + title + ".md"
+                    path = site.in_source_dir(path)
+                    doc = Jekyll::Document.new(path, { :site => site, :collection => sai_feed })
+                    doc.data['title'] = title
+                    doc.data['blog'] = "Source Allies Blog"
+                    doc.data['desc'] = desc
+                    doc.data['link'] = link
+                    doc.data['date'] = newdate
+                    sai_feed.docs << doc
+                end
             end
         end
 
         cgw_feed = Jekyll::Collection.new(site, 'cgw_feed')
         site.collections['cgw_feed'] = cgw_feed
-        url = "https://cecilgwilliams.wordpress.com/feed"
-        open(url) do |rss|
+        feed_url = "https://cecilgwilliams.wordpress.com/feed"
+        open(feed_url) do |rss|
             feed = RSS::Parser.parse(rss)
             feed.items.each do |item|
                 title = item.title
